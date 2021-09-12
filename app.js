0dd6597a4d27c06a43dc3dcca7e306bc64d36591
Leigh <leigh79712@hotmail.com>
2021-09-12 22:16:18 +0200

    RWD 設計

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const links = document.querySelectorAll("body ul li a");
-let revealerpoint = 140;
+const links = document.querySelectorAll("body li a");
+
+const navslide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  burger.addEventListener("click", () => {
+    // Toggle Nav
+    nav.classList.toggle("nav-active");
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.7}s `;
+      }
+    });
+    //burger Tansition
+    burger.classList.toggle("toggle");
+  });
+};
+
+navslide();
+
 // ======================================================
 // loading
+let revealerpoint = 140;
 window.addEventListener("scroll", reveal);
 reveal();
 
